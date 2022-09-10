07a63ffacbff8b63954de48b5dd3ea7d75e07d4c
ZARMACARON\a-soleymani <Solimaniali90@gmail.com>
2022-09-10 10:25:17 +0430

    no message

--- core/src/main/java/com/zar/core/tools/api/enums/AuthorizationType.kt
+++ core/src/main/java/com/zar/core/tools/api/enums/AuthorizationType.kt
@@ -2,6 +2,10 @@ package com.zar.core.tools.api.enums
 
 /**
  * Create by Mehrdad Latifi on 8/21/2022
+ * Create by Mehrdad Latifi on 8/21/2022
+ * Create by Mehrdad Latifi on 8/21/2022
+ * Create by Mehrdad Latifi on 8/21/2022
+ * Create by Mehrdad Latifi on 8/21/2022
  */
 
 enum class AuthorizationType {
--- core/src/main/java/com/zar/core/tools/extensions/DoubleExtension.kt
+++ core/src/main/java/com/zar/core/tools/extensions/DoubleExtension.kt
@@ -2,6 +2,9 @@ package com.zar.core.tools.extensions
 
 /**
  * Create by Mehrdad Latifi on 8/6/2022
+ * Create by Mehrdad Latifi on 8/6/2022
+ * Create by Mehrdad Latifi on 8/6/2022
+ * Create by Mehrdad Latifi on 8/6/2022
  */
 
 import java.text.DecimalFormat
