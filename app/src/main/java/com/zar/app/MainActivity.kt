dc1e5089916fb5128e00a59ea6ca18f942eb4182
m-latifi <m-latifi@zarmacaron.com>
2022-09-11 18:11:13 +0430

    نسخه 1

--- app/src/main/java/com/zar/app/MainActivity.kt
+++ app/src/main/java/com/zar/app/MainActivity.kt
@@ -2,15 +2,10 @@ package com.zar.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import com.zar.core.tools.extensions.toSolarDate
-import java.time.LocalDateTime
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val s: LocalDateTime? = LocalDateTime.of(1988,8,22,0,0,0)
-        Toast.makeText(this, "" + s.toSolarDate()?.getFullDate(), Toast.LENGTH_LONG).show()
     }
 }
