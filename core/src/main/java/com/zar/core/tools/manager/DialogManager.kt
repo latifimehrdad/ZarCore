077dccd91e5c904c6f1d35cbcd04996ff4e56bdc
m-latifi <m6.latifi@gmail.com>
2022-11-16 16:06:37 +0330

    تغییر ساخت دیالوگ

--- core/src/main/java/com/zar/core/tools/manager/DialogManager.kt
+++ core/src/main/java/com/zar/core/tools/manager/DialogManager.kt
@@ -12,16 +12,19 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Inject
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class DialogManager(@ApplicationContext private val context: Context) {
+class DialogManager @Inject constructor(@ApplicationContext private val context: Context) {
 
-    //---------------------------------------------------------------------------------------------- createDialog
-    @Provides
-    @Singleton
-    fun createDialog(layout: Int, verticalGravity: Int, verticalMargin : Int): Dialog {
+    //---------------------------------------------------------------------------------------------- createDialogHeightWrapContent
+    fun createDialogHeightWrapContent(
+        layout: Int,
+        verticalGravity: Int,
+        verticalMargin: Int
+    ): Dialog {
         val dialog = Dialog(context)
         dialog.setCancelable(false)
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
@@ -40,7 +43,29 @@ class DialogManager(@ApplicationContext private val context: Context) {
         window.attributes = lp
         return dialog
     }
-    //---------------------------------------------------------------------------------------------- createDialog
+    //---------------------------------------------------------------------------------------------- createDialogHeightWrapContent
+
+
+    //---------------------------------------------------------------------------------------------- createDialogHeightMatchParent
+    fun createDialogHeightMatchParent(layout: Int): Dialog {
+        val dialog = Dialog(context)
+        dialog.setCancelable(false)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(layout)
+        val lp = WindowManager.LayoutParams()
+        val window = dialog.window
+        val back = ColorDrawable(Color.TRANSPARENT)
+        val inset = InsetDrawable(back, 50)
+        window!!.setBackgroundDrawable(inset)
+        lp.copyFrom(window.attributes)
+        lp.width = WindowManager.LayoutParams.MATCH_PARENT
+        lp.height = WindowManager.LayoutParams.WRAP_CONTENT
+        lp.horizontalMargin = 50f
+        lp.verticalMargin = 50f
+        window.attributes = lp
+        return dialog
+    }
+    //---------------------------------------------------------------------------------------------- createDialogHeightMatchParent
 
 
 }
