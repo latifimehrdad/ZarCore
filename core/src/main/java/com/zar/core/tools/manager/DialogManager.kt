c39bd9f4f75a5d495978add7898c248359f0234b
m-latifi <m6.latifi@gmail.com>
2022-11-16 15:32:37 +0330

    تغییر ساخت دیالوگ

--- core/src/main/java/com/zar/core/tools/manager/DialogManager.kt
+++ core/src/main/java/com/zar/core/tools/manager/DialogManager.kt
@@ -2,6 +2,9 @@ package com.zar.core.tools.manager
 
 import android.app.Dialog
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.InsetDrawable
 import android.view.Window
 import android.view.WindowManager
 import dagger.Module
@@ -13,21 +16,27 @@ import dagger.hilt.components.SingletonComponent
 @InstallIn(SingletonComponent::class)
 class DialogManager(@ApplicationContext private val context: Context) {
 
-    //______________________________________________________________________________________________ createDialog
-    fun createDialog(layout: Int): Dialog {
+    //---------------------------------------------------------------------------------------------- createDialog
+    fun createDialog(layout: Int, verticalGravity: Int, verticalMargin : Int): Dialog {
         val dialog = Dialog(context)
         dialog.setCancelable(false)
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
         dialog.setContentView(layout)
         val lp = WindowManager.LayoutParams()
         val window = dialog.window
-        lp.copyFrom(window!!.attributes)
+        val back = ColorDrawable(Color.TRANSPARENT)
+        val inset = InsetDrawable(back, 50)
+        window!!.setBackgroundDrawable(inset)
+        lp.copyFrom(window.attributes)
         lp.width = WindowManager.LayoutParams.MATCH_PARENT
         lp.height = WindowManager.LayoutParams.WRAP_CONTENT
+        lp.gravity = verticalGravity
+        lp.horizontalMargin = 50f
+        lp.y = verticalMargin
         window.attributes = lp
         return dialog
     }
-    //______________________________________________________________________________________________ createDialog
+    //---------------------------------------------------------------------------------------------- createDialog
 
 
 }
