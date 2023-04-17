ff2f606b0eff67e228054aac55b96c22db96b50f
m-latifi <m3.latifi@gmail.com>
2023-04-17 15:42:09 +0430

    تغییر ماه و سال در دیالوگ انتخاب تاریخ

--- core/src/main/java/com/zar/core/tools/hilt/HiltProviders.kt
+++ core/src/main/java/com/zar/core/tools/hilt/HiltProviders.kt
@@ -52,6 +52,8 @@ class HiltProviders {
         loggingInterceptor: HttpLoggingInterceptor
     ) = OkHttpClient()
         .newBuilder()
+        .readTimeout(30, TimeUnit.SECONDS)
+        .writeTimeout(30, TimeUnit.SECONDS)
         .connectTimeout(30, TimeUnit.SECONDS)
         .addInterceptor(interceptor)
         .addNetworkInterceptor(loggingInterceptor)
