87a6345567046268246f23fb5f72869b9fcb18c1
zar <m3.latifi@gmail.com>
2023-07-17 08:59:57 +0330

    رفع باگ ورود بایومتریک

--- core/src/main/java/com/zar/core/tools/BiometricTools.kt
+++ core/src/main/java/com/zar/core/tools/BiometricTools.kt
@@ -1,11 +1,8 @@
 package com.zar.core.tools
 
 import android.content.Context
-import android.content.Intent
-import android.provider.Settings
 import androidx.biometric.BiometricManager
 import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
-import androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL
 import androidx.biometric.BiometricPrompt
 import com.zar.core.R
 import dagger.Module
@@ -22,28 +19,24 @@ class BiometricTools @Inject constructor(
     //---------------------------------------------------------------------------------------------- checkDeviceHasBiometric
     fun checkDeviceHasBiometric(biometricPrompt : BiometricPrompt) : String? {
         val biometricManager = BiometricManager.from(context)
-        when (biometricManager.canAuthenticate(BIOMETRIC_STRONG)) {
+        return when (biometricManager.canAuthenticate(BIOMETRIC_STRONG)) {
             BiometricManager.BIOMETRIC_SUCCESS -> {
                 showBiometricDialog(biometricPrompt)
+                null
             }
+
             BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> {
-                return context.getString(R.string.biometricNoHardware)
+                context.getString(R.string.biometricNoHardware)
             }
+
             BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
-                val enrollIntent = Intent(Settings.ACTION_BIOMETRIC_ENROLL).apply {
-                    putExtra(
-                        Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
-                        BIOMETRIC_STRONG or DEVICE_CREDENTIAL
-                    )
-                }
-                context.startActivity(enrollIntent)
-                return null
+                context.getString(R.string.biometricErrorNoneEnrolled)
             }
+
             else -> {
-                return context.getString(R.string.biometricNoHardware)
+                context.getString(R.string.biometricNoHardware)
             }
         }
-        return null
     }
     //---------------------------------------------------------------------------------------------- checkDeviceHasBiometric
 
@@ -52,7 +45,7 @@ class BiometricTools @Inject constructor(
     //---------------------------------------------------------------------------------------------- showBiometricDialog
     private fun showBiometricDialog(biometricPrompt : BiometricPrompt) {
         val promptInfo = BiometricPrompt.PromptInfo.Builder()
-            .setTitle("ورود با اثر انگشت")
+            .setTitle("ورود از طریق بایومتریک")
             .setNegativeButtonText("انصراف")
             .build()
         biometricPrompt.authenticate(promptInfo)
