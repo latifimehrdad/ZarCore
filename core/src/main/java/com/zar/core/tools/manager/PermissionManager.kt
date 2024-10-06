532a8b6868f0faad31f4515ea8288c25b35420ff
zar <m-latifi@zarmacaron.com>
2024-10-06 15:43:12 +0330

    fix PermissionManager

--- core/src/main/java/com/zar/core/tools/manager/PermissionManager.kt
+++ core/src/main/java/com/zar/core/tools/manager/PermissionManager.kt
@@ -9,11 +9,7 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Inject
-import javax.inject.Singleton
 
-/**
- * Created by m-latifi on 8/9/2023.
- */
 
 @Module
 @InstallIn(SingletonComponent::class)
